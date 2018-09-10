da1653cc71e391a5ba1a546feca253a520faa518
Nidhi <nidhimidha@ku.edu>
2018-09-09 19:34:10 -0500

    Changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Jimmy Wrangler Data Explorer',
+    author='Nidhi EECS 731',
+    license='MIT',
+)
